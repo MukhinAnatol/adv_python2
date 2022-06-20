0eef83a734c98341586d0b6ad19722b585741c28
MukhinAnatol <anatoliymukhin30@gmail.com>
2022-06-20 19:48:58 +1000

    created main, functions moved separate files

--- regular_expressions/reg_expr.py
+++ regular_expressions/reg_expr.py
@@ -1,30 +1,16 @@
-from pprint import pprint
-import csv
-with open("phonebook_raw.csv", encoding='utf-8') as f:
-  rows = csv.reader(f, delimiter=",")
-  contacts_list = list(rows)
-#pprint(contacts_list)
-
 import re
 def format_csv(list):
 
   phone_pattern_ext = re.compile(r"(\+7|8)+\s?\(?(\d{3})\)?\s?-?(\d{3})-?(\d{2})-?(\d+)\s\(?[а-я]+\.\s([0-9]+)\)?")
   phone_pattern = re.compile(r"(\+7|8)+\s?\(?(\d{3})\)?\s?-?(\d{3})-?(\d{2})-?(\d+)")
-  name_pattern = re.compile(r"([А-Я]{1}[а-я]+)\s?\,?([А-Я]{1}[а-я]+)\s?\,*([А-Я]{1}[а-я]+)?\,*")
+  name_pattern_1 = re.compile(r"([А-Я][а-я]+)\s?\,?([А-Я][а-я]+).([А-Я][а-я]+)\,+")
+  name_pattern_2 = re.compile(r"^([А-Я][а-я]+)\s([А-Я][а-я]+),")
 
   for index, line in enumerate(list):
     line = ','.join(line)
-    #print(line)
     line = re.sub(phone_pattern, r'+7(\2)\3-\4-\5', line)
     line = re.sub(phone_pattern_ext, r'+7(\2)\3-\4-\5 доб.\6', line)
-    line = re.sub(name_pattern, r'\1,\2,\3,', line)
+    line = re.sub(name_pattern_1, r'\1,\2,\3,', line)
+    line = re.sub(name_pattern_2, r'\1,\2', line)
     list[index] = line.split(',')
-    #print(line)
-  return(list)
-
-formatted_list = format_csv(contacts_list)
-#pprint(formatted_list)
-
-with open("phonebook.csv", "w", encoding='utf-8') as f:
-  datawriter = csv.writer(f, delimiter=',')
-  datawriter.writerows(formatted_list)
+  return list
