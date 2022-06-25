b2632cef071333384c7ca43fdb849f93b5e9a7e3
MukhinAnatol <100564440+MukhinAnatol@users.noreply.github.com>
2022-06-25 11:14:01 +1000

    Delete iter_gen directory

--- iter_gen/gen.py
+++
@@ -1,8 +0,0 @@
-
-def Flat_Generator(data):
-    for element in data:
-        if not isinstance (element, list):
-            yield element
-        else:
-            for item in Flat_Generator(element):
-                yield item
